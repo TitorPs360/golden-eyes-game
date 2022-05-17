746a02e952def2d217baa86a2787b6092fbe25d8
TitorPs360 <TitorPs360Gamer@gmail.com>
2022-05-17 22:17:56 +0700

    Update locale type

--- pages/howto.tsx
+++ pages/howto.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import Footer from '../components/Footer';
 import Image from 'next/image';
@@ -167,9 +167,9 @@ const Howto: NextPage = () => {
   );
 };
 
-export const getServerSideProps = async ({ locale }) => ({
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common'])),
+    ...(await serverSideTranslations(locale || 'en', ['common'])),
   },
 });
 
