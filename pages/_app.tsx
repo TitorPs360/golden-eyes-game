2217757aa560ead2909cf00012e03976868549fb
TitorPs360 <TitorPs360Gamer@gmail.com>
2022-05-19 21:18:20 +0700

    Add Google analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 import { appWithTranslation } from 'next-i18next';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDkzArZu1DlPWvRGL3DGcX-cQXatpG4hAs',
+  authDomain: 'golden-eyes-game.firebaseapp.com',
+  projectId: 'golden-eyes-game',
+  storageBucket: 'golden-eyes-game.appspot.com',
+  messagingSenderId: '583929792801',
+  appId: '1:583929792801:web:b385f853f841c966815660',
+  measurementId: 'G-G4TW33XJQ4',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
