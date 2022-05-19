c7d7978ab3a5b52218aff8ee51d00627fb98b906
TitorPs360 <TitorPs360Gamer@gmail.com>
2022-05-19 17:55:34 +0700

    fix missing module

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'th'],
   },
   react: { useSuspense: false },
-  localePath: path.resolve('./public/static/locales'),
+  localePath: path.resolve('./public/locales'),
 };
